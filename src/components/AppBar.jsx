8dcdb69e2abc4ce03cb15d48e2e2f4a3c59dc1a8
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2024-05-21 14:23:57 +0330

    "Added react-icons dependency and imported RiSearch2Line icon in AppBar component"

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,10 +1,11 @@
+import { RiSearch2Line } from "react-icons/ri"; 
 
 const AppBar = () => {
   return (
-    <div className="flex justify-between items-center ">
+    <div className="flex justify-between items-center mt-11 px-5">
         <div>Digitize</div>
         <div className="text-2xl font-bold text-slate-900">ساعت هوشمند</div>
-        
+        <RiSearch2Line className="rounded bg-slate-100 w-4 h-4 shadow-md" />
     </div>
   )
 }
