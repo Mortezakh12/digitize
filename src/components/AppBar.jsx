90b65a5f2027306bf557bd772e9e43ceee865f6d
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2024-05-21 14:08:59 +0330

    font vazir changed

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,12 @@
+
+const AppBar = () => {
+  return (
+    <div className="flex justify-between items-center ">
+        <div>Digitize</div>
+        <div className="text-2xl font-bold text-slate-900">ساعت هوشمند</div>
+        
+    </div>
+  )
+}
+
+export default AppBar
